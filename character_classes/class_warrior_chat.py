65dda72d854e520b2a8d7eeb2b1d830d93b765c0
Taylor Putman <fishputman@gmail.com>
2025-09-02 12:14:44 -0400

    added ability functions to character class

--- character_classes/class_warrior_chat.py
+++ character_classes/class_warrior_chat.py
@@ -35,54 +35,6 @@ class Warrior(Character):
             },
         }
 
-    # --- Ability activation ---
-    def special_ability(self, turn):
-        print("\nWhich special ability would you like to use?")
-        for key, ability in self.abilities.items():
-            print(f"{key}. {ability['name']}")
-
-        choice = input("Choose special ability: ")
-
-        if choice in self.abilities:
-            self._activate_ability(self.abilities[choice], turn)
-        else:
-            print("Invalid choice")
-
-    def _activate_ability(self, ability, turn):
-        if self.special_ability_active is None and self.special_ability_cooldown == 0:
-            self.special_ability_active = ability
-            self.special_ability_turn = turn
-            self.special_ability_cooldown = ability["cooldown"]
-            ability["apply"]()
-        else:
-            print("Special ability already active or on cooldown!")
-
-    # --- Ability state management ---
-    def check_special_ability(self, turn):
-        # Cooldown ticking
-        if self.special_ability_cooldown > 0:
-            self.special_ability_cooldown -= 1
-
-        # Handle active ability expiring
-        if self.special_ability_active:
-            ability = self.special_ability_active
-            if turn >= self.special_ability_turn + ability["duration"]:
-                ability["remove"]()
-                # If ability has a post-effect, activate it
-                if ability["post_effect"]:
-                    self.post_effect_active = ability["post_effect"]
-                    self.post_effect_turn = turn
-                    self.post_effect_active["apply"]()
-                # Reset active ability
-                self.special_ability_active = None
-
-        # Handle post-effect expiring
-        if self.post_effect_active:
-            if turn >= self.post_effect_turn + self.post_effect_active["duration"]:
-                self.post_effect_active["remove"]()
-                self.post_effect_active = None
-                self.post_effect_turn = None
-
     # --- Rage ---
     def _apply_rage(self):
         self.attack_power += 20
